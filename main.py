582423ec98120acc63e586b54c30e29c04ae4fba
Meloy9992 <noir99@yandex.ru>
2022-09-23 12:42:30 +0300

    Add getSorted method Add delete from db method Add write to db method Add deleteFile method

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import telebot
 from telebot import types
 import psycopg2
 from config import host, user, password, db_name
+import json
 
 full_name = ''
 phone_number = 0
 social_network = list('')
 isClear = True
+listUsers = dict()
+data = {
+    "users": []
+}
 
 bot = telebot.TeleBot("")
+
+
+class Users:
+    def __init__(self, full_name, phone_number, social_network):
+        self.full_name = full_name
+        self.phone_number = phone_number
+        self.social_network = social_network
+
 
 def addToDatabase():
     global isClear
@@ -38,6 +51,7 @@ def addToDatabase():
 
 def getAllUser():
     global isClear
+    # global listUsers
     try:
         connection = psycopg2.connect(
             host=host,
@@ -55,7 +69,45 @@ def getAllUser():
                 return question
             for row in users1:
                 if row[3] != '':
-                    answer += "Id= " + str(row[0]) + " Имя= " + row[1] + " Телефон= " + str(row[2]) + " Социальные сети= " + row[3] + "\n"
+                    answer += "Id= " + str(row[0]) + " Имя= " + row[1] + " Телефон= " + str(
+                        row[2]) + " Социальные сети= " + row[3] + "\n"
+                    listUsers[row[0]] = Users(row[1], row[2], row[3])
+                else:
+                    answer += "Id= " + str(row[0]) + " Имя= " + row[1] + " Телефон= " + str(row[2]) + "\n"
+                    listUsers[row[0]] = Users(row[1], row[2], row[3])
+            isClear = False
+            return answer
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL", _ex)
+    finally:
+        if connection:
+            connection.commit()
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
+
+
+def getAllUserSorted():
+    global isClear
+    # global listUsers
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM users ORDER BY full_names")
+            users1 = cursor.fetchall()
+            answer = ''
+            if len(users1) == 0:
+                isClear = True
+                question = 'Ничего не найдено'
+                return question
+            for row in users1:
+                if row[3] != '':
+                    answer += "Id= " + str(row[0]) + " Имя= " + row[1] + " Телефон= " + str(
+                        row[2]) + " Социальные сети= " + row[3] + "\n"
                 else:
                     answer += "Id= " + str(row[0]) + " Имя= " + row[1] + " Телефон= " + str(row[2]) + "\n"
             isClear = False
@@ -68,6 +120,7 @@ def getAllUser():
             connection.close()
             print("[INFO] PostgreSQL connection closed")
 
+
 def delete(id):
     try:
         connection = psycopg2.connect(
@@ -77,8 +130,8 @@ def delete(id):
             database=db_name
         )
         with connection.cursor() as cursor:
-            sql ="""DELETE FROM users WHERE id_users = %s"""
-            cursor.executemany(sql, id)
+            sql = """DELETE FROM users WHERE id_users = %s"""
+            cursor.execute(sql, (id,))
 
     except Exception as _ex:
         print("[INFO] Error while working with PostgreSQL", _ex)
@@ -91,20 +144,27 @@ def delete(id):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Для того чтобы начать введите любую из команд: /add, ")
+    bot.reply_to(message, "Для того чтобы начать введите любую из команд: /add, /getAll, /getSorted, /delete, /write, /deleteFile")
 
 
-# @bot.message_handler(func=lambda m: True)
 @bot.message_handler(commands=['add'])
 def echo_all(message):
     # if message.text == '/add':
     bot.send_message(message.from_user.id, "Введите ФИО")
     bot.register_next_step_handler(message, add_full_name)
 
+
 @bot.message_handler(commands=['getAll'])
 def echo_all(message):
     # if message.text == '/getAll':
     bot.send_message(message.from_user.id, getAllUser())
+
+
+@bot.message_handler(commands=['getSorted'])
+def echo_all(message):
+    # if message.text == '/getAll':
+    bot.send_message(message.from_user.id, getAllUserSorted())
+
 
 @bot.message_handler(commands=['delete'])
 def echo_all(message):
@@ -116,9 +176,28 @@ def echo_all(message):
         bot.send_message(message.from_user.id, "Введите Id для удаления")
         bot.register_next_step_handler(message, deleted)
 
+
+@bot.message_handler(commands=['write'])
+def echo_all(message):
+    # if message.text == '/write':
+    print(getAllUser())
+    data['users'].clear()
+    for id, users in listUsers.items():
+        data['users'].append(users.__dict__)
+        writeToFile(data, 'data.json')
+
+
+@bot.message_handler(commands=['deleteFile'])
+def echo_all(message):
+    # if message.text == '/write':
+    data['users'].clear()
+    deleteFromFile(data, 'data.json')
+
+
 def deleted(message):
     delete(message.text)
     bot.send_message(message.from_user.id, "Удалено")
+
 
 def add_full_name(message):
     global full_name
@@ -176,7 +255,22 @@ def callback_worker(call):
         bot.register_next_step_handler(call.message, add_full_name)
 
 
+def writeToFile(data, fileName):
+    data = json.dumps(data)
+    data = json.loads(str(data))
+    with open(fileName, 'w+', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii= False)
+
+
+def deleteFromFile(data, fileName):
+    data = json.dumps(data)
+    data = json.loads(str(data))
+    with open(fileName, 'w+', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii= False)
+
+
 def soc_network():
     return ', '.join(social_network)
+
 
 bot.polling()
